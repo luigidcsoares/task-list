397ebe9b9a845a8744b61230b0d6e227649532fd
Luigi D. C. Soares <luigi.soares@sga.pucminas.br>
2019-04-12 19:50:00 -0300

    ServiceWorker -> cache files

--- src/sw.js
+++ src/sw.js
@@ -1,1 +1,27 @@
-self.addEventListener('install', e => console.log('SW: instalado', e));
+// Save the files to the user's device.
+// The 'install' event is called when the ServiceWorker starts up.
+self.addEventListener('install', e => {
+  console.log('SW: install', e);
+
+  // Tells the browser that the install event is not finished until we have
+  // cached all of our files.
+  e.waitUntil(
+    caches.open('cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/manifest.webmanifest'
+      ]);
+    })
+  );
+});
+
+// Intercept requests and return the cached version instead.
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    // If cached file exists, return it.
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+  console.log('AOOOO');
+});
