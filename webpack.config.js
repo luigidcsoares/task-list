c3ac47439c6680b71a587f2fc45d41c4286f1a2d
Ana <alcvlara@sga.pucminas.br>
2019-04-21 13:18:01 -0300

    Updating some elements

--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,76 @@
-const Path = require('path');
-const htmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const WebpackServiceWorker = require('serviceworker-webpack-plugin');
+const Path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const WebpackServiceWorker = require("serviceworker-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   performance: {
     hints: false
   },
   output: {
-      path: Path.join(__dirname, '/dist'),
-      filename: 'bundle.js'
+    path: Path.join(__dirname, "/dist"),
+    filename: "bundle.js"
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-react']
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"]
+          }
         }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ["css-loader"]
+        })
+      },
+      {
+        test: /\.(png|jpg|gif|ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        use: [
+          {
+            loader: "file-loader"
+          }
+        ]
       }
-    }, {
-      test: /\.css$/,
-      use: ExtractTextPlugin.extract(
-        {
-          fallback: 'style-loader',
-          use: ['css-loader']
-        }
-      )
-    }, {
-      test: /\.(png|jpg|gif|ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-      use: [
-        {
-          loader: 'file-loader'
-        }
-      ]
-    }]
+    ]
   },
   plugins: [
     new htmlWebpackPlugin({
       hash: true,
-      filename: 'index.html', // Target html.
-      favicon: './public/images/icons/favicon.ico', // Favicon.
-      template: './public/index.html' // Source html.
+      filename: "index.html", // Target html.
+      favicon: "./public/images/icons/favicon.ico", // Favicon.
+      template: "./public/index.html" // Source html.
     }),
 
-    new ExtractTextPlugin({ filename: 'style.css' }),
+    new ExtractTextPlugin({ filename: "style.css" }),
     new WebpackPwaManifest({
       inject: true,
-      filename: 'manifest.webmanifest',
-      name: 'PWA Task List',
-      short_name: 'Task List',
-      descriptions: 'A simple PWA for keep tracking your tasks.',
-      theme_color: '#2196f3',
-      background_color: '#9f9f9f',
-      display: 'standalone',
-      scope: '.',
-      start_url: '/',
+      filename: "manifest.webmanifest",
+      name: "PWA Task List",
+      short_name: "Task List",
+      lang: "pt-BR",
+      descriptions: "A simple PWA for keep tracking your tasks.",
+      theme_color: "#2196f3",
+      background_color: "#9f9f9f",
+      display: "standalone",
+      scope: ".",
+      start_url: "/",
       icons: [
         {
-          src: Path.join(__dirname, 'public/images/icons/icon.png'),
+          src: Path.join(__dirname, "public/images/icons/icon.png"),
           sizes: [72, 96, 128, 144, 152, 192, 384, 512]
         }
       ]
     }),
 
-    new WebpackServiceWorker({ entry: Path.join(__dirname, 'src/sw.js') })
+    new WebpackServiceWorker({ entry: Path.join(__dirname, "src/sw.js") })
   ]
-}
+};
